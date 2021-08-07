051663cb8955d93ba01adba5970251a9fc4f222e
Nouman Amir <noumanamir453@gmail.com>
2021-08-08 03:12:55 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, Container, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import { Facebook } from 'react-bootstrap-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <Navbar bg="dark" expand="md" variant='dark'>
+      <Navbar.Brand href="#home">
+        <Facebook width='2em' height='2em'/>
+      </Navbar.Brand>
+      
+      <Form>
+        <FormControl
+          type="search"
+          placeholder="Search Facebook"
+          className="mr-2 rounded-pill"
+          aria-label="Search"
+        />
+      </Form>
+
+      <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+      <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="me-auto mx-5">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#watch">Watch</Nav.Link>
+      <Nav.Link href="#marketplace">Marketplace</Nav.Link>
+      <Nav.Link href="#groups">Groups</Nav.Link>
+      </Nav>
+      </Navbar.Collapse>
+
+  </Navbar>
+  )
 }
 
 export default App;
