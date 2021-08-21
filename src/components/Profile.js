1c114e9708ba86ca1370a82ec1b9eaf26f8bb2e8
Nouman Amir <noumanamir453@gmail.com>
2021-08-21 16:15:30 +0500

    Improving profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,13 +5,29 @@ import PostShow from './PostShow'
 const Profile = ({postsData}) => {
     const [{user}, dispatch] = useStateValue()
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('Cover Images uploaded')
+    }
+
     return (
         <div>
-        <div><img src='https://png.pngtree.com/background/20210712/original/pngtree-modern-double-color-futuristic-neon-background-picture-image_1181573.jpg' className='w-full h-80' alt='image'></img></div>
+
+        <form onSubmit={handleSubmit}>
+        <div className="mb-1">
+            <div className="relative border-dotted h-32 rounded-lg border-2 border-blue-700 bg-gray-100 flex justify-center items-center">
+                <div className="absolute">
+                    <div className="flex flex-col items-center"><span className="block text-gray-400 font-normal">Upload Cover Image</span></div>
+                    </div> <input type="file" className="h-full w-full opacity-0" name=""></input>
+            </div>
+        </div>
+        <div className="mt-3 text-right"><button type='submit' className="ml-2 h-10 w-32 bg-blue-600 rounded text-white hover:bg-blue-700">Upload</button></div>
+        </form>
+
         <div className='text-center text-2xl font-bold'>{user.displayName}</div>
         <br></br>
         <div className='flex justify-center mx-4'>
-        <div className='text-cente font-bold text-lg w-80'>
+        <div className='text-cente font-bold text-lg w-80 hidden md:block'>
         <div className='text-2xl py-2 underline'>Intro</div>
         <div>Went to xyz</div>
         <div>Went to xyz</div>
